9ef74d76c91f4d7eafcd87a89a922d680010342a
zhao <ziwen.zhao@kemi.uu.se>
2022-02-28 20:01:31 +0100

    Documenting

---
+++ ex4.5 documenting/docs/index.rst
@@ -0,0 +1,21 @@
+.. test_quote documentation master file, created by
+   sphinx-quickstart on Mon Feb 28 19:31:09 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to test_quote's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+.. automodule:: simple_math
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- ex4.5 documenting/simple_math.py
+++ ex4.5 documenting/simple_math.py
@@ -1,10 +1,37 @@
 """
- A collection of simple math operations
+A collection of simple math operations
 
 Methods define here
-----------
+-------------------
 simple_add(...)
+
+
+simple_sub(...)
+
+
+simple_mult(...)
+
+
+simple_div(...)
+
+
+simple_div(...)
+
+
+poly_first(...)
+
+    
+def poly_second(...)
+
+       
+"""
+ 
+
+
+def simple_add(a,b):
+    '''
     simple_add(a,b)
+    
     Parameters
     ----------
     a,b : float optional
@@ -13,13 +40,21 @@ simple_add(...)
     Returns 
     ------- 
     the sum of the input a,b 
+    
     Examples
     --------
     >>> simple_math.simple_add(5,10)
-    15
+    >>> 15
+    
+    '''
+      
+    return a+b
 
-simple_sub(...)
-    simple_sub(a,b)   
+def simple_sub(a,b):
+    '''    
+
+    simple_sub(a,b) 
+    
     Parameters
     ----------
     a,b : float optional
@@ -28,59 +63,67 @@ simple_sub(...)
     Returns 
     ------- 
     the Substraction of the input a,b 
+
     Examples
     --------
     >>> simple_math.simple_sub(5,10)
-    -5
+    >>> -5
 
-simple_mult(...)
-    simple_mult(a,b)   
+    '''
+    
+    return a-b
+
+def simple_mult(a,b):
+    '''
+    simple_mult(a,b)  
+    
     Parameters
     ----------
     a,b : float optional
     int,float or array_like int,float
     if a,b are array_like variables, the size should match
+    
     Returns 
     ------- 
     the Multiplacation of the input a,b 
+    
     Examples
     --------
     >>> simple_math.simple_mult(5,10)
-    50
+    >>> 50
+    
+    '''
+    
+    return a*b
 
-simple_div(...)
+def simple_div(a,b):
+    '''
     simple_div(a,b)   
+
     Parameters
     ----------
     a,b : float optional
     int,float or array_like int,float
     if a,b are array_like variables, the size should match
+    
     Returns 
     ------- 
     the Division of the input a,b 
+    
     Examples
     --------
     >>> simple_math.simple_div(5,10)
-    0.5
+    >>> 0.5
 
-simple_div(...)
-    simple_div(a,b)   
-    Parameters
-    ----------
-    a,b : float optional
-    int,float or array_like int,float
-    if a,b are array_like variables, the size should match
-    Returns 
-    ------- 
-    the Division of the input a,b 
-    Examples
-    --------
-    >>> simple_math.simple_div(5,10)
-    0.5
+    '''
+    
+    return a/b
 
-poly_first(...)
+def poly_first(x, a0, a1):
+    '''
     poly_first(x, a0, a1)
-    retuen the value of a1*x + a0
+    retuen the value of a1*x + a0  
+
     Parameters
     ----------
     a1,a0 : int,float optional
@@ -91,11 +134,17 @@ poly_first(...)
     Examples
     --------
     >>> simple_math.poly_first(5,10,15)
-    85
-    
-def poly_second(...):
+    >>> 85
+
+    '''
+    return a0 + a1*x
+
+def poly_second(x, a0, a1, a2):
+    '''
     poly_second(x, a0, a1, a2)
+
     retuen the value of a2*x**2 + a1*x + a0
+    
     Parameters
     ----------
     a1,a1,a0 : int,float optional
@@ -106,29 +155,10 @@ def poly_second(...):
     Examples
     --------
     >>> simple_math.poly_second(5,10,15,10)
-    335
-       
-"""
- 
+    >>> 335
 
-
-def simple_add(a,b):
-  
-    return a+b
-
-def simple_sub(a,b):
-    return a-b
-
-def simple_mult(a,b):
-    return a*b
-
-def simple_div(a,b):
-    return a/b
-
-def poly_first(x, a0, a1):
-    return a0 + a1*x
-
-def poly_second(x, a0, a1, a2):
+    '''
+    
     return poly_first(x, a0, a1) + a2*(x**2)
 
 # Feel free to expand this list with more interesting mathematical operations...
